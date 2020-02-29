ac2076ddc510f196eb93be517fff995cc0b20efd
Colin Suess <colin.suess@gmail.com>
2020-02-29 12:46:36 -0800

    Committing base

---
+++ ChessV.Games/Pieces/ChessWIthPointsPieces.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessV.Games
+{
+	#region Queen
+	[PieceType("OldQueen", "Chess with Points")]
+	public class OldQueen : PieceType
+	{
+		public OldQueen(string name, string notation, int midgameValue, int endgameValue, string preferredImageName = null) :
+			base("OldQueen", name, notation, midgameValue, endgameValue, preferredImageName)
+		{
+			AddMoves(this);
+		}
+
+		public static new void AddMoves(PieceType type)
+		{
+			Rook.AddMoves(type);
+			Bishop.AddMoves(type);
+		}
+	}
+	#endregion
+
+}
